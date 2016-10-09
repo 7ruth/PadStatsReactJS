45589282b2d5ddd86578d6139443795844d0f0fb
Martin Terskin <martinas.terskin@gmail.com>
2016-10-09 17:32:25 -0400

    env var set up

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 var config = {};
 
-
-config.twitter.AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID || 'AUTH0_CLIENT_ID';
-config.twitter.AUTH0_DOMAIN=  process.env.AUTH0_DOMAIN || 'AUTH0_DOMAIN';
+config.AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID || 'AUTH0_CLIENT_ID';
+config.AUTH0_DOMAIN=  process.env.AUTH0_DOMAIN || 'AUTH0_DOMAIN';
 config.GAPI_KEY=  process.env.GAPI_KEY || 'GAPI_KEY';
 
 module.exports = config;
